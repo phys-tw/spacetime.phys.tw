bed956f88f6a20c12e6952732d131406c55cc3f2
AceChen <acechen@cirx.org>
2017-10-16 18:23:37 -0400

    Revert "v31"
    This reverts commit 1e7802822bcafd239c20cf024e6889ab38e8a297.

--- old/v31/s.php
+++
@@ -1,8 +0,0 @@
-<?php
-$folder = array('', '1_main1', '2_main2', '3_series1', '4_series2', '5_art_design', '6_other');
-$uploaddir = $folder[(int)$_GET['g']];
-$filename = $_GET['f'];
-$uploadhttp = "http://phys.tw/~spacetime/v31/upload/$uploaddir/$filename";
-echo "<script>location.href='./upload/$uploaddir/$filename';</script>";
-?>
-<html></html>
