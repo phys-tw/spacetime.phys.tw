656cbf60c6c20ea9fd85b6df03f2cb167be9a58b
黃偉翔 <huangweixiang@huangweixiangdeMacBook-Pro.local>
2023-09-27 14:49:38 +0800

    封面封底

---
+++ vol_38/page.js
@@ -0,0 +1,10 @@
+function topCover()
+{
+    const all = document.getElementById("all");
+    all.innerHTML = "<div><img src=\"封面(RGB).png\" class=\"cover\"/></div> <div class=\"homePageLink\"><a href=\"index.html\">回首頁</a></div>"
+}
+function bottomCover()
+{
+    const all = document.getElementById("all");
+    all.innerHTML = "<div><img src=\"封底(RGB).png\" class=\"cover\"/></div> <div class=\"homePageLink\"><a href=\"index.html\">回首頁</a></div>"
+}
